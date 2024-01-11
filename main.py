3343ef49d3a302034a9b5d045e58efcedd88ac09
nohjuhyeon <njh2720@gmail.com>
2024-01-11 11:26:31 +0900

    FASTAPI

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ app = FastAPI()
 #     await settings.initialize_database()
 
 from routes.admin_main import router as event_router                   
-# # from routes.mypage import router as second_router
-# # from routes.plan_trip import router as users_router
+# from routes.mypage import router as second_router
+from routes.plan_trip import router as users_router
 
 app.include_router(event_router, prefix="/admin")
-# # app.include_router(second_router, prefix="/mypage")
-# # app.include_router(users_router, prefix="/plan_trip")
+# app.include_router(second_router, prefix="/mypage")
+app.include_router(users_router, prefix="/plan_trip")
 
 from fastapi import Request                                
 from fastapi.templating import Jinja2Templates              
---
+++ routes/plan_trip.py
@@ -0,0 +1,75 @@
+from fastapi import APIRouter
+from starlette.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+
+
+router = APIRouter()
+
+templates = Jinja2Templates(directory="templates/")
+
+## 여행 계획 추천
+@router.post("/reco_trip_plan") # 펑션 호출 방식
+async def list_post(request:Request):
+    await request.form()
+    print(dict(await request.form()))
+    return templates.TemplateResponse(name="plan_trip/reco_trip_plan.html", context={'request':request})
+
+@router.get("/reco_trip_plan") # 펑션 호출 방식
+async def list_post(request:Request):
+    await request.form()
+    print(dict(await request.form()))
+    return templates.TemplateResponse(name="plan_trip/reco_trip_plan.html", context={'request':request})
+
+## 여행 계획
+@router.post("/trip_plan") # 펑션 호출 방식
+async def list_post(request:Request):
+    await request.form()
+    print(dict(await request.form()))
+    return templates.TemplateResponse(name="plan_trip/trip_plan.html", context={'request':request})
+
+@router.get("/trip_plan") # 펑션 호출 방식
+async def list_post(request:Request):
+    await request.form()
+    print(dict(await request.form()))
+    return templates.TemplateResponse(name="plan_trip/trip_plan.html", context={'request':request})
+
+## 교통 예약
+@router.post("/reserve_transfer") # 펑션 호출 방식
+async def list_post(request:Request):
+    await request.form()
+    print(dict(await request.form()))
+    return templates.TemplateResponse(name="plan_trip/reserve_transfer.html", context={'request':request})
+
+@router.get("/reserve_transfer") # 펑션 호출 방식
+async def list_post(request:Request):
+    await request.form()
+    print(dict(await request.form()))
+    return templates.TemplateResponse(name="plan_trip/reserve_transfer.html", context={'request':request})
+
+
+## 숙소 예약
+@router.post("/reserve_dorm") # 펑션 호출 방식
+async def list_post(request:Request):
+    await request.form()
+    print(dict(await request.form()))
+    return templates.TemplateResponse(name="plan_trip/reserve_dorm.html", context={'request':request})
+
+@router.get("/reserve_dorm") # 펑션 호출 방식
+async def list_post(request:Request):
+    await request.form()
+    print(dict(await request.form()))
+    return templates.TemplateResponse(name="plan_trip/reserve_dorm.html", context={'request':request})
+
+## 투어 예약
+@router.post("/reserve_event") # 펑션 호출 방식
+async def list_post(request:Request):
+    await request.form()
+    print(dict(await request.form()))
+    return templates.TemplateResponse(name="plan_trip/reserve_event.html", context={'request':request})
+
+@router.get("/reserve_event") # 펑션 호출 방식
+async def list_post(request:Request):
+    await request.form()
+    print(dict(await request.form()))
+    return templates.TemplateResponse(name="plan_trip/reserve_event.html", context={'request':request})
