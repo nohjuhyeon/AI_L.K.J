70ba2841fed28fe55a4ed306dc71e693508b4808
nohjuhyeon <njh2720@gmail.com>
2024-01-15 14:45:59 +0900

    selenium

--- routes/plan_trip.py
+++ routes/plan_trip.py
@@ -8,6 +8,11 @@ router = APIRouter()
 
 templates = Jinja2Templates(directory="templates/")
 
+from databases.connections import Database
+from models.reserve_transfer_car import transfer_car_list
+collection_transfer_car_list = Database(transfer_car_list)
+
+
 
 ## 여행 계획 추천
 @router.post("/reco_trip_plan") # 펑션 호출 방식
@@ -33,6 +38,9 @@ async def list_post(request:Request):
 async def list_post(request:Request):
     await request.form()
     print(dict(await request.form()))
+    car_list = await collection_transfer_car_list.get_all()
+    print(car_list)
+
     return templates.TemplateResponse(name="plan_trip/trip_plan.html", context={'request':request})
 
 ## 교통 예약
@@ -46,8 +54,25 @@ async def list_post(request:Request):
 async def list_post(request:Request):
     await request.form()
     print(dict(await request.form()))
+
     return templates.TemplateResponse(name="plan_trip/reserve_transfer.html", context={'request':request})
 
+## 교통 예약
+@router.post("/reserve_transfer_car") # 펑션 호출 방식
+async def list_post(request:Request):
+    await request.form()
+    car_list = await collection_transfer_car_list.get_all()
+    print(car_list)
+    print(dict(await request.form()))
+    return templates.TemplateResponse(name="plan_trip/reserve_transfer_car.html", context={'request':request})
+
+@router.get("/reserve_transfer_car") # 펑션 호출 방식
+async def list_post(request:Request):
+    await request.form()
+    car_list = await collection_transfer_car_list.get_all()
+    print(car_list)
+    print(dict(await request.form()))
+    return templates.TemplateResponse(name="plan_trip/reserve_transfer_car.html", context={'request':request})
 
 ## 숙소 예약
 @router.post("/reserve_dorm") # 펑션 호출 방식
