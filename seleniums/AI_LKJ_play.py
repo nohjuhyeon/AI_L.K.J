89e5da37e837e3dcd00341e611acfd908ea36022
nohjuhyeon <njh2720@gmail.com>
2024-01-17 17:31:09 +0900

    aaa

--- seleniums/AI_LKJ_play.py
+++ seleniums/AI_LKJ_play.py
@@ -32,6 +32,27 @@ html = browser.page_source                          # - html 파일 받음(and �
 
 from selenium.webdriver.common.by import By          # - 정보 획득
 # browser.save_screenshot('./formats.png')           
+time.sleep(5)
+login = browser.find_element(by=By.CSS_SELECTOR,value="body > div.container > div > div > header > ul > li:nth-child(1) > a")
+login.click()
+time.sleep(3)
+insert = browser.find_element(by=By.CSS_SELECTOR,value="body > div > main > div > form > div > div:nth-child(5) > button:nth-child(2)")
+insert.click()
+time.sleep(2)
+email_input = browser.find_element(by=By.CSS_SELECTOR,value="#email")
+password_input = browser.find_element(by=By.CSS_SELECTOR,value="#password")
+password_check = browser.find_element(by=By.CSS_SELECTOR,value="#password_check")
+user_name = browser.find_element(by=By.CSS_SELECTOR,value="#user_name")
+user_date = browser.find_element(by=By.CSS_SELECTOR,value="#user_birth")
+user_phone = browser.find_element(by=By.CSS_SELECTOR,value="#phone")
+user_address = browser.find_element(by=By.CSS_SELECTOR,value="#address")
 
-
+email_input.send_keys("njh2205@gmail.com")
+password_input.send_keys("1234")
+password_check.send_keys("12345")
+user_name.send_keys("주현")
+user_date.send_keys("19980205")
+user_phone.send_keys("01012345678")
+user_address.send_keys("서울")
+time.sleep(5)
 browser.quit()                                      # - 브라우저 종료
